953fce39f3ec53d27791d8ebe537af9f613a4f7f
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-03-24 23:39:26 -0400

    added seed files and roughed in some hbs templets

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
-const Article = require('../models/article');
-// Routes for files in public folder
+const { Article, Comments, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const articleData = await Article.findAll();
+
+    const articles = JSON.parse(JSON.stringify(articleData));
+    res.render('article-details', {
+      articles,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
